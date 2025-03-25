6a2f55dd3b0d0d24e5a35addd8b40562399023aa
Craig <craig@highcube.org>
2025-03-25 19:47:52 -0400

    clean up for coros-specific bits

--- lib/omniauth/strategies/coros.rb
+++ lib/omniauth/strategies/coros.rb
@@ -12,10 +12,23 @@ module OmniAuth
         :token_url => 'https://open.coros.com/oauth2/accesstoken'
       }
 
-      uid { raw_info['user']['id'] }
+      def build_access_token
+        token_response = super
+        response_body = token_response.params
+        @open_id = response_body['openId']
+        token_response
+      end
+
+      def uid
+        @open_id
+      end
 
       def raw_info
-        @raw_info ||= access_token.get('/coros/userinfosim').parsed
+        @raw_info ||= access_token.get("/coros/userinfosim?token=#{access_token.token}&openId=#{@open_id}").parsed
+      end
+
+      extra do
+        { raw_info: raw_info, open_id: @open_id }
       end
     end
   end
